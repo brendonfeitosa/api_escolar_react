e107e1489f2b85fd10ecfa0f012c6c91f097ce58
Brendon Gustavo Feitosa <Brendon Gustavo Feitosa>
2025-10-28 19:24:26 -0300

    Adicionando arquivos .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuração do Vite
 export default defineConfig({
   plugins: [react()],
+
+  // Durante o desenvolvimento local (npm run dev),
+  // o Vite redireciona chamadas /api para o Flask local.
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000', // Flask local
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
+  // Quando o projeto for buildado, será hospedado no Render,
+  // então as chamadas devem usar a URL pública do Flask.
+  // Isso você define no código do React (ex: fetch).
 })
